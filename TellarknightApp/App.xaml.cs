bd66a2219a46cf4fae87a53f905237568d793214
Metalkon <MetalkonSC@hotmail.com>
2024-01-15 00:38:32 -0400

    fixed window size

--- TellarknightApp/App.xaml.cs
+++ TellarknightApp/App.xaml.cs
@@ -8,5 +8,22 @@
 
             MainPage = new MainPage();
         }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            const int newWidth = 1400;
+            const int newHeight = 750;
+
+            window.Width = newWidth;
+            window.Height = newHeight;
+            window.MinimumWidth = newWidth;
+            window.MinimumHeight = newHeight;
+            window.MaximumWidth = newWidth;
+            window.MaximumHeight = newHeight;
+
+            return window;
+        }
     }
 }
