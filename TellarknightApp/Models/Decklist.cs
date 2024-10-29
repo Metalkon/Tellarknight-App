d7a474141a56f692d0537bbfa466bbefc846ec62
Metalkon <MetalkonSC@hotmail.com>
2024-10-29 16:35:40 -0300

    Added the ability to view decklist

--- TellarknightApp/Models/Decklist.cs
+++ TellarknightApp/Models/Decklist.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using TellarknightApp.Cards;
 
 namespace TellarknightApp.Models
 {
     public class Decklist
     {
+        public event Action? Action;
         public List<Card> MainDeck { get; set; }
         public List<Card> ExtraDeck { get; set; }
 
@@ -15,6 +12,39 @@ namespace TellarknightApp.Models
         {
             MainDeck = new List<Card>();
             ExtraDeck = new List<Card>();
+        }
+
+        public void BuildDecklist(SupportedCards supportedCards)
+        {
+            MainDeck.Clear();
+            ExtraDeck.Clear();
+
+            foreach (Card card in supportedCards.Cards)
+            {
+                if (card.Quantity > 3 && card is not Level4 && card is not EmptyCard)
+                {
+                    card.Quantity = 3;
+                }
+                if (card.Quantity > 0 && card.Role != "Extra Deck")
+                {
+                    for (int i = 0; i < card.Quantity; i++)
+                    {
+                        MainDeck.Add(card);
+                    }
+                }
+                if (card.Quantity > 0 && card.Role == "Extra Deck")
+                {
+                    for (int i = 0; i < card.Quantity; i++)
+                    {
+                        ExtraDeck.Add(card);
+                    }
+                }
+            }
+            while (MainDeck.Count < 40)
+            {
+                MainDeck.Add(new EmptyCard());
+            }
+            Action?.Invoke();
         }
     }
 }
